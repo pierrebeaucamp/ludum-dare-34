d7a9c0d6270fb14d6697d194f511e500dbe61a3c
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-12-11 18:46:23 -0500

    First small steps towards webgl

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,10 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
+import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Console (CONSOLE(), log)
+import Graphics.WebGLAll (runWebGL)
+import Prelude (Unit())
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main :: Eff (console :: CONSOLE) Unit
+main = runWebGL "glcanvas" (\s -> log s) \ context -> do
+    log "Canvas initialized"
