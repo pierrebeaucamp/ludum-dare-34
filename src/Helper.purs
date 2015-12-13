8406d44912a5874bb238414051671e35256afc81
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-12-12 21:54:51 -0500

    Reset repository for pivot

--- src/Helper.purs
+++
@@ -1,6 +0,0 @@
-module Helper where
-
-import Data.Time
-
-unpackMilliseconds :: Milliseconds -> Number
-unpackMilliseconds (Milliseconds n) = n
--- test/Main.purs
+++ test/Main.purs
@@ -1,4 +1,4 @@
-module Main where
+module Test.Main where
 
 import Prelude
 import Control.Monad.Eff
@@ -6,4 +6,4 @@ import Control.Monad.Eff.Console
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  log "You should add some tests."
